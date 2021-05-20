ce680dd42d985ebb3d74deb4964447a2ce2f3dbc
xd-elon <alandevelope@gmail.com>
2021-05-20 16:07:58 -0300

    blocks

--- block.ts
+++ block.ts
@@ -1,1 +1,29 @@
+import sha256 from 'crypto-js/sha256';
 
+
+class Block {
+
+    Index: number
+    Timestamp: Date
+    Data: any
+    Hash: string
+    PreviousHash: string
+    Nonce: number
+    Duration: number
+
+
+    constructor (
+        index: number,
+        data: any,
+        previousHash: string = "",
+        ) {
+ 
+        this.Index = index;
+        this.Timestamp = new Date();
+        this.Data = data;
+        this.Hash = this.computerHash();
+        this.PreviousHash = previousHash;
+        this.Nonce = 0;
+        this.Duration = 0;
+    }
+}
